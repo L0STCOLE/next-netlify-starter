6aa7b5135d385a302a1265928de12c6856eced8a
L0STCOLE <93286109+L0STCOLE@users.noreply.github.com>
2022-04-11 19:03:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,12 @@ export default function Home() {
       <main>
         <Header title="Welcome to Gods Helping Hands" />
         <p className="description">
-          4 Sisters Private Care Givers <code></code>
-          24 Houre Services
+          4 Sisters Private Care Givers /n<code></code>
+          24 Houre Services/n
           Contact Estella for more information at (831)-531-2323
+/n Offering pravite care in the Houston Area are prices range from $25 per hour to $30 per hour. Our Sister's 
+/n specalize in giving care to your loved one's with the upmost attenction and care. Please contact us to book
+/n a time with one of our Sister's.
         </p>
       </main>
 
